76f13d4481cd4600fc11995de590b84afdb268b7
lee01302503 <lee01302503@naver.com>
2020-09-12 16:13:22 +0900

    report2 D pratice

---
+++ report3(3-3, 2)/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* made by Prof. Lee
+*/
+int main(void)
+{
+  char ch;
+
+  printf("Code : ");
+  scanf("%c", &ch);
+  printf("%c Code of Ascii code %d", ch, ch);
+
+  return 0;
+}
