f46a5683ed181a29ef8d34dae49d1cb0e80c4510
lee01302503 <lee01302503@naver.com>
2020-09-12 15:51:37 +0900

    report2(3-(9-1))

---
+++ report(3-2, No.3)/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* made by Prof. Lee
+*/
+int main(void)
+{
+  char ch;
+  int in;
+
+  printf("Code : ");
+  scanf("%c", &ch);
+  printf("Ascii code : ");
+  scanf("%d", &in);
+  printf("%c of Ascii code %d.", ch, in);
+
+  return 0;
+
+
+}
