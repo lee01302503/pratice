1470a5014c27dba064069edfda6ae81b1822e64a
lee01302503 <lee01302503@naver.com>
2020-09-12 14:37:33 +0900

    report 2(3-3)

---
+++ report2(1-(3-2)/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* made by Prof. Lee
+*/
+int main(void)
+{
+  int kor = 70, eng = 80, mat = 90;
+  int tot = 240;
+
+  tot = kor + eng + mat;
+  printf("Korea : %d, English : %d, Math : %d\n", 70, 80, 90);
+  printf("Toatal : %d\n", kor + eng + mat);
+
+
+  return 0;
+}
