87a6e5550c00ae46130ebcc133a7c45c510bcfef
lee01302503 <lee01302503@naver.com>
2020-09-13 15:45:56 +0900

    report 4-2

---
+++ report 4-2 3/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* made by Prof. Lee
+*/
+int main(void)
+{
+  int hour, min, sec;
+  double time = 3.76;
+
+  hour = (int)time;
+  time -= hour;
+  time *= 60.0;
+  min = (int)time;
+  time -= min;
+  time *= 60.0;
+  sec = (int)time;
+  printf("3.76 time is %dhour %dmin %dsec\n", hour, min, sec);
+
+}
