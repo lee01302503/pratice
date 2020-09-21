6e2bd27320582970d06d0e5278bd45b9dd17df16
lee01302503 <lee01302503@naver.com>
2020-09-21 23:46:32 +0900

    Report3(1.C)

---
+++ Report3(1-c)/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* made by Prof. Lee
+*/
+int main(void)
+{
+  int age = 25, chest = 95;
+  char size;
+  if (age < 20)
+  {
+	if (chest < 85)
+	{
+	  size = 'S';
+	}
+	else if ((85 <= chest) && (chest < 95))
+	{
+	  size = 'M';
+	}
+	else
+	{
+	  size = 'L';
+	}
+  }
+  else
+  {
+	if (chest < 90)
+	{
+	  size = 'S';
+	}
+	else if ((90 <= chest) && (chest < 100))
+	{
+	  size = 'M';
+	}
+	else
+	{
+	  size = 'L';
+	}
+  }
+  printf(" Size : %c\n", size);
+  return 0;
+ }
